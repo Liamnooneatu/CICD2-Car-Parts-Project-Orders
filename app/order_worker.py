afb76e7c52d6ba137a8e218a82b334dd63a49a38
Liamnooneatu <118723698+Liamnooneatu@users.noreply.github.com>
2026-01-09 13:49:36 +0000

    setup orders similar to service a to test

---
+++ app/order_worker.py
@@ -0,0 +1,32 @@
+import aio_pika
+import asyncio
+import json
+import os
+
+RABBIT_URL = os.getenv("RABBIT_URL")
+EXCHANGE_NAME = "events_topic"
+
+
+async def main():
+    if not RABBIT_URL:
+        raise RuntimeError("RABBIT_URL is not set. Run: set -a; source .env; set +a")
+
+    conn = await aio_pika.connect_robust(RABBIT_URL)
+    ch = await conn.channel()
+
+    ex = await ch.declare_exchange(EXCHANGE_NAME, aio_pika.ExchangeType.TOPIC)
+
+    queue = await ch.declare_queue("order_events_queue", durable=True)
+    await queue.bind(ex, routing_key="order.*")
+
+    print("Listening for order events (routing key: 'order.*')...")
+
+    async with queue.iterator() as q:
+        async for msg in q:
+            async with msg.process():
+                data = json.loads(msg.body)
+                print("Order Event:", msg.routing_key, data)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
